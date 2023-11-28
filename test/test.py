f55931b89ac1c38bec22394272b0c1dfea2067d1
Madhavee <madhavee.kadivar@fintso.com>
2023-11-28 12:37:25 +0530

    [Madhavee] : Improved model

--- test/test.py
+++ test/test.py
@@ -1,5 +1,5 @@
 import requests 
 
-resp = requests.post("http://localhost:5000/api/predict", files={'file': open('seven.png', 'rb')})
+resp = requests.post("http://127.0.0.1:5000/api/predict", files={'file': open('three.png', 'rb')})
 
 print(resp.text)
--- torch_utils.py
+++ torch_utils.py
@@ -1,49 +1,47 @@
 import io
 import torch 
 import torch.nn as nn 
+import torch.nn.functional as F 
 import torchvision.transforms as transforms 
 from PIL import Image
 
-# load model
+class SimpleCNN(nn.Module):
+    def __init__(self):
+        super(SimpleCNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.fc1 = nn.Linear(64 * 7 * 7, 128)
+        self.fc2 = nn.Linear(128, 10)
 
-class NeuralNet(nn.Module):
-    def __init__(self, input_size, hidden_size, num_classes):
-        super(NeuralNet, self).__init__()
-        self.input_size = input_size
-        self.l1 = nn.Linear(input_size, hidden_size) 
-        self.relu = nn.ReLU()
-        self.l2 = nn.Linear(hidden_size, num_classes)  
-    
     def forward(self, x):
-        out = self.l1(x)
-        out = self.relu(out)
-        out = self.l2(out)
-        # no activation and no softmax at the end
-        return out
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 64 * 7 * 7)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
 
-input_size = 784 # 28x28
-hidden_size = 500 
-num_classes = 10
-model = NeuralNet(input_size, hidden_size, num_classes)
+model = SimpleCNN()
 
-PATH = "mnist_nn.pth"
+PATH = "mnist.pth"
 model.load_state_dict(torch.load(PATH))
 model.eval()
 
 # image -> tensor
+# image -> tensor
 def transform_image(image_bytes):
-    transform = transforms.Compose([transforms.Grayscale(num_output_channels=1),
-                                    transforms.Resize((28,28)),
-                                    transforms.ToTensor(),
-                                    transforms.Normalize((0.1307,),(0.3581,))])
-
+    transform = transforms.Compose([
+    transforms.Grayscale(num_output_channels=1),  # Convert to grayscale if needed
+    transforms.Resize((28, 28)),  # Resize the image to the expected size
+    transforms.ToTensor(),  # Convert to a PyTorch tensor
+    transforms.Normalize((0.5,), (0.5,))  # Normalize as done during training
+    ])
     image = Image.open(io.BytesIO(image_bytes))
     return transform(image).unsqueeze(0)
 
 # predict
 def get_prediction(image_tensor):
-    images = image_tensor.reshape(-1, 28*28)
-    outputs = model(images)
-        # max returns (value ,index)
+    outputs = model(image_tensor)
     _, predicted = torch.max(outputs.data, 1)
     return predicted
